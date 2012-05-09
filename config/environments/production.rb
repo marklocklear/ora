1d981d92f5ce8cc9b9856949bedba2cc117b5bcb
J. Mark Locklear <marklocklear@gmail.com>
2012-05-09 16:30:21 -0400

    wintas

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,6 +11,7 @@ Opa2::Application.configure do
 
   # Specifies the header that your server uses for sending files
   config.action_dispatch.x_sendfile_header = "X-Sendfile"
+  config.serve_static_assets = true
   # For nginx:
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
 
